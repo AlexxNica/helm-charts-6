3ca8845482ce13205b2ebeb22b35fe0b1d9db3e4
carldanley <carldanley@gmail.com>
2017-08-31 23:34:11 -0400

    Added CI

--- build-packages.sh
+++ build-packages.sh
@@ -1,25 +1,26 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 # variables
 BASE_DIRECTORY=.
 CHARTS_DIRECTORY="${BASE_DIRECTORY}/charts"
 
+# remove any previously generated charts
+rm -rf $CHARTS_DIRECTORY
+
 # ensure our charts directory exists
-if [ ! -d "${CHARTS_DIRECTORY}" ]; then
-  mkdir -p $CHARTS_DIRECTORY
-fi
+mkdir -p $CHARTS_DIRECTORY
 
 # begin generating the charts
 for DIRECTORY in `ls $BASE_DIRECTORY`;do
-  if [ -d "${BASE_DIRECTORY}/${DIRECTORY}" ]; then
-    helm lint "${BASE_DIRECTORY}/${DIRECTORY}"
+  if [ "$DIRECTORY" = "charts" ]; then
+    continue;
+  elif [ -d "${BASE_DIRECTORY}/${DIRECTORY}" ]; then
     helm package "${BASE_DIRECTORY}/${DIRECTORY}" -d $CHARTS_DIRECTORY --debug
   fi
 done
 
-# create the index.yaml
-if [ -f "${BASE_DIRECTORY}/index.yaml" ]; then
-  helm repo index --merge "${$CHARTS_DIRECTORY}/index.yaml" $CHARTS_DIRECTORY
-else
-  helm repo index $CHARTS_DIRECTORY
-fi
+# download the existing index.yaml file
+curl http://helm.carldanley.com/index.yaml > $CHARTS_DIRECTORY/index.yaml
+
+# re-index the repository (using the existing index.yaml as the base)
+helm repo index --merge "${CHARTS_DIRECTORY}/index.yaml" $CHARTS_DIRECTORY
